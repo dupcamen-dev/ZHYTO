def07e49ec3afd5a55da0a279e8af87ab5644cda
dupcamen-dev <dupcamen.dev@gmail.com>
2026-05-16 12:58:08 +0300

    Configure GitHub Pages static export with GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isExport = process.env.EXPORT === 'true'
+
 const nextConfig = {
+  ...(isExport ? {
+    output: 'export',
+    basePath: '/ZHYTO',
+    trailingSlash: true,
+  } : {}),
   typescript: {
     ignoreBuildErrors: true,
   },
