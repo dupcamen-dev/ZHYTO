a98a79559f23ea84c678672a4c8fa2c7aaeac3dd
dupcamen-dev <dupcamen.dev@gmail.com>
2026-05-17 21:24:21 +0300

    Move text to bottom of card with justify-end pb-24

--- app/page.tsx
+++ app/page.tsx
@@ -410,7 +410,7 @@ export default function Home() {
                     priority
                   />
                 </div>
-                <div className="relative z-10 p-8 lg:p-12 flex flex-col justify-center pt-48 sm:pt-56">
+                <div className="relative z-10 p-8 lg:p-12 flex flex-col justify-end pb-24 lg:pb-32">
                 <motion.p
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
