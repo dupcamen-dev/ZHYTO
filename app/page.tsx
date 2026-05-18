dd10c77e04fa61c2d7864aae8c390b4979aa311a
dupcamen-dev <dupcamen.dev@gmail.com>
2026-05-18 21:26:39 +0300

    Image container 400% -> 700% to offset smaller text

--- app/page.tsx
+++ app/page.tsx
@@ -467,7 +467,7 @@ export default function Home() {
             <span className="text-white block tracking-[0.2em]">Dumplings</span>
             <span className="font-script text-foreground text-[0.6em] uppercase relative inline-block tracking-[0.15em]">
               with soul
-              <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-[46%] w-[400%] h-[400%] -z-10">
+              <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-[46%] w-[700%] h-[700%] -z-10">
                 <Image
                   src={img("/images/hero-soul-bg.png")}
                   alt=""
