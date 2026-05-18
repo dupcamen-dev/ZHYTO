8229cbc65d629e5b7ffa0c0565f659a4b840c986
dupcamen-dev <dupcamen.dev@gmail.com>
2026-05-18 21:22:25 +0300

    Dumplings tracking 0.08em -> 0.2em

--- app/page.tsx
+++ app/page.tsx
@@ -464,7 +464,7 @@ export default function Home() {
             transition={{ duration: 0.8, delay: 0.4 }}
             className="text-7xl sm:text-8xl md:text-9xl lg:text-[10rem] font-serif font-light leading-[1.1] mb-10 relative tracking-[0.05em]"
           >
-            <span className="text-white block tracking-[0.08em]">Dumplings</span>
+            <span className="text-white block tracking-[0.2em]">Dumplings</span>
             <span className="font-script text-foreground text-[0.8em] uppercase relative inline-block tracking-[0.15em]">
               with soul
               <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-[46%] w-[400%] h-[400%] -z-10">
